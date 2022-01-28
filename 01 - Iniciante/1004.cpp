c8f9ba1a7914d685bf72ed1ce45847c319850658
Luis Felipe de Oliveira Fernandes <luisfelipeofernandes@gmail.com>
2022-01-27 23:22:31 -0300

    Problema 1004 - Produto Simples

---
+++ 01 - Iniciante/1004.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int val1, val2, PROD;
+    
+    scanf("%d",&val1);
+    scanf("%d",&val2);
+    
+    PROD = val1*val2;
+    
+    printf("PROD = %d\n",PROD);
+    return 0;
+}
