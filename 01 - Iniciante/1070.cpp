863e089c955bd19dc5687874bf680aad93de4bf9
Lui5felipe <luisfelipeofernandes@gmail.com>
2022-01-29 21:03:14 -0300

    Problema 1070 - Seis Números Ímpares

---
+++ 01 - Iniciante/1070.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    cin >> n;
+    if(n%2==0)
+        n++;
+    for(i=0;i<6;i++)
+    {
+        cout << n << endl;
+        n+=2;
+    }
+    return 0;
+}
