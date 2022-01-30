220250d57a643235015443a1c326d217b1ba0f8c
Lui5felipe <luisfelipeofernandes@gmail.com>
2022-01-30 13:26:01 -0300

    Problema 1044 - Múltiplos

---
+++ 01 - Iniciante/1044.c
@@ -0,0 +1,35 @@
+/*
+*File:main.c
+*Author:LuisFelipe
+*
+*Createdon10deAgostode2014,00:08
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+*
+*/
+int main()
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  
+  if(a<b)
+    {
+      if(b%a==0)
+        printf("Sao Multiplos\n");
+      else
+        printf("Nao sao Multiplos\n");
+    }
+  else
+    {
+      if(a%b==0)
+        printf("Sao Multiplos\n");
+      else
+        printf("Nao sao Multiplos\n");
+    }
+
+return 0;
+}
