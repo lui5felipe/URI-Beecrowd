1021c5dd43bc788ba65b901fb25fcab428bea9ce
Lui5felipe <luisfelipeofernandes@gmail.com>
2022-02-06 19:06:34 -0300

    Problema 1759 -  Ho Ho Ho

---
+++ 01 - Iniciante/1759.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int N,i;
+    scanf("%d",&N);
+    for(i=0;i<N-1;i++)
+    {
+        printf("Ho ");
+    }
+    printf("Ho!\n");
+    return 0;
+}
