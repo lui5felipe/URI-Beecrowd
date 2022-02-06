0e126744223aa45a407ff505edb5a1878cc9e803
Lui5felipe <luisfelipeofernandes@gmail.com>
2022-02-06 16:52:54 -0300

    Problema 1159 - Soma de Pares Consecutivos

---
+++ 01 - Iniciante/1159.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+    int x, i;
+
+    while(scanf("%d",&x) && x)
+    {
+        if (x%2==0)
+            printf("%d\n",(x+(x+2)+(x+4)+(x+6)+(x+8)));
+        else
+            printf("%d\n",((x+1)+(x+3)+(x+5)+(x+7)+(x+9)));
+    }
+    return 0;
+}
+
