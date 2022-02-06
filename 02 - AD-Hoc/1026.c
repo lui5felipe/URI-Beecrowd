c1fc3706b62c58c988e86e2a71128bb327497f2a
Lui5felipe <luisfelipeofernandes@gmail.com>
2022-02-06 19:09:50 -0300

    Problema 1026 - Carrega ou não Carrega?

---
+++ 02 - AD-Hoc/1026.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+    unsigned long long int a, b;
+    while (scanf("%llu %llu", &a, &b) != EOF)
+        printf("%llu\n", a ^ b);
+
+    return 0;
+}
